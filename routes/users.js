341e659b2f2922be462858d171c25fa2f07447b8
Artem Sokolov <stasbasov@yandex.ru>
2022-04-03 23:38:46 +0300

    Изменения для второй итерации

--- routes/users.js
+++ routes/users.js
@@ -8,9 +8,9 @@ const {
   getUserInfo,
 } = require('../controllers/users');
 
-routes.get('users/me', getUserInfo);
+routes.get('/users/me', getUserInfo);
 
-routes.patch('users/me', celebrate({
+routes.patch('/users/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     email: Joi.string().required().email(),
